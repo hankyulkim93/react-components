a8877c158f0d58cbbe0cbcf1bd2ae4be7c88a3fd
hankyulkim93 <hankyulkim93@gmail.com>
2019-07-29 13:19:55 -0700

    completed first two checkboxes

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 // TODO
+var GroceryList = () => (
+    <div><h1>My Grocery List</h1>
+    <ul>
+        <li>Orange Juice</li>
+        <li>Chips</li>
+    </ul>
+    </div>
+
+  );
+  ReactDOM.render(<GroceryList />, document.getElementById("app"));
