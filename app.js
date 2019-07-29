2711503997bc3b4c0f50d0c89f01866cd144ce0d
hankyulkim93 <hankyulkim93@gmail.com>
2019-07-29 16:57:51 -0700

    done minus one step... need to move on to recast.ly within pair time box

--- app.js
+++ app.js
@@ -1,11 +1,78 @@
 // TODO
-var GroceryList = () => (
-    <div><h1>My Grocery List</h1>
+/* var GroceryList = (props) => (
+    // <div>
+    //     <h1>My Grocery List</h1>
     <ul>
         <li>Orange Juice</li>
-        <li>Chips</li>
+        <li>Peaches</li>
+        <Cucumbers />
+        <Kale /> 
+        <li>{props.todos[0]}</li>
+        <li>{props.todos[1]}</li>
+        <li>{props.todos[2]}</li>
     </ul>
-    </div>
+    // </div>
+);
 
-  );
-  ReactDOM.render(<GroceryList />, document.getElementById("app"));
+*/
+
+class GroceryListItem extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <li>{this.props.groceryItem}</li>
+        );
+    }
+}
+
+var GroceryList = (props) => (
+    <ul>
+        {props.groceryItems.map( (groceryItem) =>
+            <GroceryListItem groceryItem={groceryItem} />
+        )}
+    </ul>
+);
+
+// var App = () => (
+//     <div>
+//         <h1>My Grocery List</h1>
+//         <GroceryList todos={['apples', 'burritos', 'mandu']}/>
+//     </div>
+// )
+
+ReactDOM.render(<GroceryList groceryItems={['apples', 'burritos', 'mandu']}/>, document.getElementById("app"));
+// var GroceryList = (props) => {
+//     var onListItemClick = (even) => {
+//         console.log('I got clicked');
+//     };
+
+//     return (
+//       <ul>
+//         <li onClick={onListItemClick}>{props.todos[0]}</li>
+//         <li>{props.todos[1]}</li>
+//         <li>{props.todos[2]}</li>
+//       </ul>
+//     );
+// };
+
+// var GroceryListItem = () => (
+//     <div>
+//         <h1>My Grocery List</h1>
+//         <GroceryList todos={['apples', 'burritos', 'mandu']}/>
+//     </div>
+// )
+
+// var Cucumbers = () => (
+//     <div>
+//         <li>Cucumbers</li>
+//     </div>
+// );
+// var Kale = () => (
+//     <div>
+//         <li>Kale</li>
+//     </div>
+// );
+
